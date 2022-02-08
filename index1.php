cd317a2228511be9c9cc8ab786f2be0c58c4bb94
Borejson1 <99260129+Borejson1@users.noreply.github.com>
2022-02-08 13:30:12 +0100

    Add files via upload

---
+++ index1.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <title>PowrutowcySieZesrali</title>
+    <link rel="stylesheet" href="style1.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Lato&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="css/fontello.css" type="text/css">
+</head>
+<body>
+    <div class="header"> <br>
+        <img src="logo.png" alt="logo"> <br><br><br><br>
+    </div>
+
+    <div class="main">
+        <div class="jeden">
+           <b>Powrutowcy</b> <br> Powrutowcy to ludzie głęboko zapatrzeni w powrut <br> Roberta Kubicy do Formuły 1, w co raczej <br> nikt zdrowo myślący nie wierzy.
+           Więc można <br> powiedzieć, że są to swojego rodzaju fanatycy Roberta <br>którzy od dawna nie myślą racjonalnie czym <br> zakłamują samych siebie
+        </div>
+        <div class="dwa">
+           <b>Inicjatywa</b> <br> Powstała więc inicjatywa która ma na celu <br> zdelegalizowanie powrutowców na mocy prawa ponieważ <br> stanowią oni poważne zagrożenie dla społeczeństwa <br>
+            i powinni być trzymani odizolowani w <br> specjalnych rezerwatach przyrodniczych <br> gdzie nie zrobią nikomu krzywd
+        </div>
+        <div class="trzy">
+           <b>Pomysłodawcy</b> <br> Stwierdziłem więc że trzeba coś zrobić z powrutowcami robiąc ową stronę internetową z petycją <br> Wszyscy działamy przeciwko roznoszeniu plagi <br> 
+           podobnie jak grupa PowrutowcySieZesrali <br> jak i cała społeczność anty-powrutowa <br> <a href="https://www.facebook.com/groups/3194289764006348"><b class="fblink">PowrutowcySieZesrali</b> </a>
+        </div> 
+    </div>
+    <div class="form"> <br><br><br><br><br><br><br><br><br><br><br><br>
+    <form action="*"> 
+        <label><b class="Mail">Adres E-Mail :</b><br><br> </label> <input type="text" name="E-Mail" /> <br><br>
+        <label><b class="Mail">Twój Nick :</b><br><br> </label> <input type="text" name="Name" /> <br><br><br>
+        <input type="submit" value="Podpisz">
+        <input type="reset" value="Wyczyść">
+   </form>
+        
+    </div>
+</body>
+</html>
